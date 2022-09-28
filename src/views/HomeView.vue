42ea43a91540615d7939d9a795e7a4a6045baf57
Christophe Paganon <cpaganon@panierquebecois.ca>
2022-09-28 19:34:05 -0400

    Move services to its own component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script>
 import Portfolio from "@/components/Portfolio.vue";
+import Services from "@/components/Services.vue";
 
 export default {
   components: {
     Portfolio,
+    Services,
   },
 };
 </script>
@@ -22,53 +24,7 @@ export default {
       </section>
       <hr class="separator" />
       <section class="services-section content-section">
-        <h2>What can I do for you?</h2>
-        <p>
-          I specialize in <strong>Wordpress Web Development</strong> but I can also help you with your marketing startegy!
-        </p>
-        <table class="services-wrapper">
-          <thead>
-            <tr>
-              <th>
-                <p class="icon">&#128640;</p>
-                <p>New Website</p>
-              </th>
-              <th>
-                <p class="icon">&#127942;</p>
-                <p>Existing Website</p>
-              </th>
-              <th>
-                <p class="icon">&#128202;</p>
-                <p>Marketing Startegy</p>
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>
-                <ul>
-                  <li>Get leads & customers</li>
-                  <li>Built on a strong base</li>
-                  <li>Best practices for Wordpress</li>
-                </ul>
-              </td>
-              <td>
-                <ul>
-                  <li>New features</li>
-                  <li>Improve speed & SEO</li>
-                  <li>Complete overhaul</li>
-                </ul>
-              </td>
-              <td>
-                <ul>
-                  <li>Identifying key metrics</li>
-                  <li>Social Media strategy</li>
-                  <li>Paid media startegy</li>
-                </ul>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+        <Services />
       </section>
       <div class="background-section-wrapper portfolio-section-background">
         <section class="portfolio-section-wrapper content-section" id="portfolio">
@@ -133,33 +89,6 @@ img:hover {
 }
 .services-section {
   text-align: center;
-}
-table {
-  margin: 40px auto;
-  max-width: 100%;
-  border: 1px solid gray;
-  border-radius: 15px;
-  border-spacing: 0px;
-}
-th:not(:last-child), td:not(:last-child) {
-  border-right: 1px solid gray;
-}
-th, td {
-  padding: 20px;
-}
-th {
-  font-size: 27px;
-  width: 33%;
-}
-td {
-  font-size: 19px;
-}
-li {
-  margin: 10px 0;
-}
-.icon {
-  font-size: 40px;
-  margin-bottom: 20px;
 }
 
 .contact-info {
