802569c35b76d2d94447b683c5f82266c882f389
Christophe Paganon <cpaganon@panierquebecois.ca>
2022-09-29 17:02:54 -0400

    Add contact form netlify submission

--- src/components/ContactMe.vue
+++ src/components/ContactMe.vue
@@ -1,15 +1,58 @@
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      form: {},
+      formResponse: '',
+    };
+  },
+  methods: {
+    encode (data) {
+      return Object.keys(data)
+        .map(
+          key => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`
+        )
+        .join("&");
+    },
+    handleSubmit() {
+      const axiosConfig = {
+        header: { "Content-Type": "application/x-www-form-urlencoded" }
+      };
+      axios.post(
+        "/",
+        this.encode({
+          "form-name": "contact-me-form",
+          ...this.form
+        }),
+        axiosConfig
+      )
+      .then(() => {
+        this.formResponse = 'Thank you for contacting me, I will get back to you by e-mail very soon.';
+      })
+      .catch(() => {
+        this.formResponse = 'There was an error while submitting the form, please contact me by e-mail or phone directly.';
+      });
+    },
+  }
+}
+</script>
+
 <template>
   <h2 class="contact-me-heading">Contact Me</h2>
   <div class="contact-me-wrapper">
     <div class="contact-me-form-wrapper contact-me-col">
       <h3>Fill up the form</h3>
-      <form name="contact-me-form" netlify>
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="name" />
+      <form name="contact-me-form" method="post" @submit.prevent="handleSubmit" netlify>
+        <input type="hidden" name="form-name" value="contact-me-form" />
+        <label for="fullName">Name:</label>
+        <input type="text" name="fullName" id="fullName" v-model="form.fullName" />
         <label for="name">E-mail:</label>
-        <input type="email" name="email" id="email" />
+        <input type="email" name="email" id="email" v-model="form.email" />
         <label for="name">Tell me about your project:</label>
-        <textarea name="description" id="description" rows="4" placeholder='Something like "I would like to add pictures from my Instagram to my existing website..."'></textarea>
+        <textarea name="description" id="description" v-model="form.description" rows="4" placeholder='Something like "I would like to add pictures from my Instagram to my existing website..."'></textarea>
+        <p>{{formResponse}}</p>
         <button class="button">Send</button>
       </form> 
     </div>
