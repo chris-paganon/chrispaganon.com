79ca36dc0566719180569ddca09ca6517aa50556
Christophe Paganon <cpaganon@panierquebecois.ca>
2022-09-30 11:25:54 -0400

    Change services to flex from table

--- src/components/Services.vue
+++ src/components/Services.vue
@@ -3,49 +3,41 @@
   <p>
     I specialize in <strong>Wordpress Web Development</strong> but I can also help you with your marketing startegy!
   </p>
-  <table class="services-wrapper">
-    <thead>
-      <tr>
-        <th>
-          <p class="icon">&#128640;</p>
-          <p>New Website</p>
-        </th>
-        <th>
-          <p class="icon">&#127942;</p>
-          <p>Existing Website</p>
-        </th>
-        <th>
-          <p class="icon">&#128202;</p>
-          <p>Marketing Startegy</p>
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>
-          <ul>
-            <li>Get leads & customers</li>
-            <li>Built on a strong base</li>
-            <li>Best practices for Wordpress</li>
-          </ul>
-        </td>
-        <td>
-          <ul>
-            <li>New features</li>
-            <li>Improve speed & SEO</li>
-            <li>Complete overhaul</li>
-          </ul>
-        </td>
-        <td>
-          <ul>
-            <li>Identifying key metrics</li>
-            <li>Social Media strategy</li>
-            <li>Paid media startegy</li>
-          </ul>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="services-wrapper">
+    <div class="service-wrapper">
+      <div class="service-title">
+        <p class="icon">&#128640;</p>
+        <p>New Website</p>
+      </div>
+      <ul>
+        <li>Get leads & customers</li>
+        <li>Built on a strong base</li>
+        <li>Best practices for Wordpress</li>
+      </ul>
+    </div>
+    <div class="service-wrapper">
+      <div class="service-title">
+        <p class="icon">&#127942;</p>
+        <p>Existing Website</p>
+      </div>
+      <ul>
+        <li>New features</li>
+        <li>Improve speed & SEO</li>
+        <li>Complete overhaul</li>
+      </ul>
+    </div>
+    <div class="service-wrapper">
+      <div class="service-title">
+        <p class="icon">&#128202;</p>
+        <p>Marketing Startegy</p>
+      </div>
+      <ul>
+        <li>Identifying key metrics</li>
+        <li>Social Media strategy</li>
+        <li>Paid media startegy</li>
+      </ul>
+    </div>
+  </div>
   <p><em>No fuss & no extra fees. I am practical and down to earth. I build useful tools to get mesurable results.</em></p>
   <router-link to="/#contact-me" class="contact-me-btn button">
     Let's Talk
@@ -53,24 +45,29 @@
 </template>
 
 <style scoped>
-table {
+.services-wrapper {
+  display: flex;
   margin: 60px auto 40px auto;
-  max-width: 100%;
   border: 1px solid gray;
   border-radius: 15px;
-  border-spacing: 0px;
 }
-th:not(:last-child), td:not(:last-child) {
+.service-wrapper {
+  width: 33%;
+  text-align: center;
+}
+.service-wrapper:not(:last-child) {
   border-right: 1px solid gray;
 }
-th, td {
-  padding: 20px;
-}
-th {
+
+.service-title {
+  padding: 20px 20px 5px 20px;
   font-size: 27px;
+  font-weight: 600;
   width: 33%;
+  margin: auto;
 }
-td {
+ul {
+  padding: 5px 20px 20px 20px;
   font-size: 19px;
 }
 li {
