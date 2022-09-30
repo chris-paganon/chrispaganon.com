fc298023e2c338e40ce651116956a679e82c534c
Christophe Paganon <cpaganon@panierquebecois.ca>
2022-09-30 14:53:27 -0400

    Basic view more button for portfolio intro

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -5,11 +5,22 @@ export default {
   data() {
     return {
       portfolio,
+      viewMoreClass: 'view-more-off',
+      viewMoreOn: false,
     };
   },
   methods: {
     getImageUrl(name) {
       return new URL(`/src/assets/images/portfolio/${name}`, import.meta.url).href;
+    },
+  },
+  watch: {
+    viewMoreOn(newViewMoreOn) {
+      if (newViewMoreOn) {
+        return this.viewMoreClass = 'view-more-on';
+      } else {
+        return this.viewMoreClass = 'view-more-off';
+      }
     }
   }
 };
@@ -17,9 +28,14 @@ export default {
 
 <template>
   <h2 class="portfolio-heading">My Portfolio</h2>
-  <p class="portfolio-intro"><strong>I built my first website in 2016</strong> for a 3D printing University project. Since then I built and managed <strong>several websites</strong> for local and international projects: a crepes catering project, a B2B massage therapy and ostheopathie service, a local bowling in Colombia and a small-scale plastic recycling community in Montréal.</p>
-  <p class="portfolio-intro">In 2020, <strong>I co-founded the first online farmer's market in Montréal</strong>: <a href="https://panierquebecois.ca" target="_blank">Panier Québécois</a>. I have been developping, improving and maintaining our website ever since (along managing our business' startegy, marketing and operations with my lovely co-founders).</p>
-  <p class="portfolio-intro">As our business doesn't require a full-time developper anymore, I am now putting my experience in web development, entrepreneurship and marketing to good use: <strong>helping you build your online presence!</strong></p>
+  <div :class="`portfolio-intro-wrapper ${viewMoreClass}`">
+    <div class="view-more-overlay" v-if="! viewMoreOn"></div>
+    <p class="portfolio-intro"><strong>I built my first website in 2016</strong> for a 3D printing University project. Since then I built and managed <strong>several websites</strong> for local and international projects: a crepes catering project, a B2B massage therapy and ostheopathie service, a local bowling in Colombia and a small-scale plastic recycling community in Montréal.</p>
+    <p class="portfolio-intro">In 2020, <strong>I co-founded the first online farmer's market in Montréal</strong>: <a href="https://panierquebecois.ca" target="_blank">Panier Québécois</a>. I have been developping, improving and maintaining our website ever since (along managing our business' startegy, marketing and operations with my lovely co-founders).</p>
+    <p class="portfolio-intro">As our business doesn't require a full-time developper anymore, I am now putting my experience in web development, entrepreneurship and marketing to good use: <strong>helping you build your online presence!</strong></p>
+  </div>
+  <div class="arrow arrow-down" @click="viewMoreOn = true" v-if="! viewMoreOn"></div>
+  <div class="arrow arrow-up" @click="viewMoreOn = false" v-if="viewMoreOn"></div>
   <h3 class="portfolio-sub-heading">Find out more about a few of my projects below:</h3>
   <div class="portfolio-wrapper">
     <router-link
@@ -49,7 +65,36 @@ img {
   border-radius: 10px;
 }
 
+.arrow {
+  width: 15px;
+  height: 15px;
+  border-right: 4px solid black;
+  border-bottom: 4px solid black;
+  margin: auto;
+  cursor: pointer;
+}
+.arrow-down {
+  transform: rotate(45deg);
+}
+.arrow-up {
+  transform: rotate(-135deg);
+}
+
+.portfolio-intro-wrapper.view-more-off {
+  position: relative;
+  max-height: 4em;
+  overflow: hidden;
+  transition: max-height 2s ease-out;
+}
+.view-more-overlay {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-image: linear-gradient(to top, rgb(255, 251, 246), transparent);
+  z-index: 10;
+}
 .portfolio-intro {
+  display: block;
   margin-bottom: 30px;
 }
 
@@ -66,8 +111,6 @@ img {
   flex-direction: column;
   justify-content: space-between;
   align-items: flex-start;
-  text-decoration: none;
-  color: black;
   overflow: hidden;
   transition: transform 1s;
 }
