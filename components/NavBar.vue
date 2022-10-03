3a3e82060d85da71f623ba917a6d56628f129ed9
Christophe Paganon <cpaganon@panierquebecois.ca>
2022-10-03 12:16:03 -0400

    Replace home NavBar link with icon

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <ul>
-      <li><NuxtLink :to="localePath({ name: 'index' })">{{ $t('NavBar.home') }}</NuxtLink></li>
+      <li><NuxtLink :to="localePath({ name: 'index' })"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g data-name="31.Home"><path d="M12 24a12 12 0 1 1 12-12 12.013 12.013 0 0 1-12 12zm0-22a10 10 0 1 0 10 10A10.011 10.011 0 0 0 12 2z"/><path d="M18.293 13.707 12 7.414l-6.293 6.293-1.414-1.414L12 4.586l7.707 7.707-1.414 1.414z"/><path d="M17 18H7v-8h2v6h6v-6h2v8z"/></g></svg></NuxtLink></li>
       <li><NuxtLink :to="localePath({ path: '/#portfolio' })">{{ $t('NavBar.portfolio') }}</NuxtLink></li>
       <li>
         <NuxtLink :to="localePath({ path: '/#contact-me' })" class="contact-me-btn button">
@@ -35,6 +35,15 @@ a:not(.contact-me-btn) {
 }
 a:not(.contact-me-btn):hover {
   letter-spacing: 2.5px;
+}
+
+svg {
+  width: 24px;
+  margin-bottom: -5px;
+  transition: width 250ms ease-out;
+}
+svg:hover {
+  width: 27px;
 }
 
 @media (max-width: 600px) {
